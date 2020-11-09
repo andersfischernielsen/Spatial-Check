0ab58cc461bcf91cfe246d6c1b3c1b2dc0ede08c
Anders Fischer-Nielsen <andersfischern@me.com>
2020-11-09 18:15:08 +0100

    Initial implementation of calling Windows COM

---
+++ Spatial Check/Spatial Check/AudioHelpers/SpatialAudioClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace AudioHelpers
+{
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("BBF8E066-AAAA-49BE-9A4D-FD2A858EA27F")]
+    interface ISpatialAudioClient
+    {
+        [PreserveSig]
+        int GetMaxDynamicObjectCount(
+            [Out][MarshalAs(UnmanagedType.U4)] out uint value);
+    }
+
+    public class SpatialAudioClient
+    {
+
+        private readonly ISpatialAudioClient spatialAudioClientInterface;
+        public int MaxDynamicObjectCount
+        {
+            get
+            {
+                Marshal.ThrowExceptionForHR(spatialAudioClientInterface.GetMaxDynamicObjectCount(out uint dynamicObjectCount));
+                return (int) dynamicObjectCount;
+            }
+        }
+
+        internal SpatialAudioClient(ISpatialAudioClient spatialAudioClientInterface)
+        {
+            this.spatialAudioClientInterface = spatialAudioClientInterface;
+        }
+    }
+}
---
+++ Spatial Check/Spatial Check/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Runtime.InteropServices;
+using System.Windows.Shapes;
+using AudioHelpers;
+using NAudio.CoreAudioApi;
+
+namespace Spatial_Check
+{
+    
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Test();
+        }
+
+        private void Test()
+        {
+            var deviceEnumerator = new MMDeviceEnumerator();
+            foreach (var d in deviceEnumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active)) { 
+                var spatialAudioClient = d.SpatialAudioClient;
+                var result = spatialAudioClient.MaxDynamicObjectCount;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
