a1a5487c88c3529c7355a3f5ea249e800302fb3b
Anders Fischer-Nielsen <andersfischern@me.com>
2020-11-10 13:54:49 +0100

    Implement tray application with companion .exe

--- Spatial Check/ConsoleApplication/ConsoleApplication.cpp
+++ Spatial Check/ConsoleApplication/ConsoleApplication.cpp
@@ -1,12 +1,74 @@
-// ConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <SpatialAudioClient.h>
 #include <wrl.h>
 #include <mmdeviceapi.h>
+#include "wintoastlib.h"
 
-int main()
+using namespace WinToastLib;
+
+class CustomHandler : public IWinToastHandler {
+public:
+    void toastActivated() const {
+        exit(0);
+    }
+
+    void toastActivated(int actionIndex) const { exit(16 + actionIndex); }
+
+    void toastDismissed(WinToastDismissalReason state) const { }
+
+    void toastFailed() const {
+        std::wcout << L"Error showing current toast" << std::endl;
+        exit(5);
+    }
+};
+
+
+void showToast(UINT32 dynamicObjectCount, bool userCheckInitiated) {
+    if (!WinToast::isCompatible()) {
+        std::wcerr << L"Error, your system in not supported!" << std::endl;
+        return;
+    }
+
+    if (dynamicObjectCount > 0 && !userCheckInitiated) return; 
+    WinToastTemplate templ(WinToastTemplate::Text02);
+    std::wstring appName = L"Spatial Check";
+    std::wstring appUserModelID = L"Spatial Check";
+    std::wstring notifyText;
+    std::wstring detailsText;
+    std::wstring attribute = L" ";
+    INT64 expiration = 5;
+    
+    WinToast::instance()->setAppName(appName);
+    WinToast::instance()->setAppUserModelId(appUserModelID);
+    if (!WinToast::instance()->initialize()) {
+        std::wcerr << L"Error, your system in not compatible!" << std::endl;
+        return;
+    }
+
+
+    if (dynamicObjectCount > 0 && userCheckInitiated) {
+        notifyText = L"Spatial sound is enabled";
+        detailsText = L"Open Sound settings to disable";
+    }
+    if (dynamicObjectCount <= 0) {
+        notifyText = L"Spatial sound is disabled";
+        detailsText = L"Open Sound settings to re-enable";
+    }
+    
+    templ.setExpiration(expiration);
+    templ.setTextField(notifyText, WinToastTemplate::FirstLine);
+    templ.setTextField(detailsText, WinToastTemplate::SecondLine);
+    templ.setAttributionText(attribute);
+
+    if (WinToast::instance()->showToast(templ, new CustomHandler(), nullptr) < 0) {
+        std::wcerr << L"Could not launch your toast notification!";
+        return;
+    }
+
+    Sleep(expiration ? (DWORD)expiration + 1000 : 10000);
+}
+
+int main(int argc, char* argv[], char* envp[])
 {
     HRESULT hr;
     Microsoft::WRL::ComPtr<IMMDeviceEnumerator> deviceEnum;
@@ -18,5 +80,7 @@ int main()
     hr = defaultDevice->Activate(__uuidof(ISpatialAudioClient), CLSCTX_INPROC_SERVER, nullptr, (void**)&spatialAudioClient);
     UINT32 count;
     spatialAudioClient->GetMaxDynamicObjectCount(&count);
-    std::cout << count << " \n";
+    
+    if (argc > 1) { showToast(count, true); }
+    else showToast(count, false);
 }
